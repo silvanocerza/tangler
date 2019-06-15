b6ebaab86838302fe7d9ac893ea00c9488540def
Silvano Cerza <silvanocerza@gmail.com>
2019-06-15 13:24:44 +0200

    Implemented lines intersections

--- Main.gd
+++ Main.gd
@@ -10,18 +10,17 @@ func _ready() -> void:
 	max_y = get_viewport().size[1]
 	graph = load("res://Graph.gd").new()
 	add_child(graph)
-	create_triangles()
+	create_edges()
 
-func create_triangles() -> void:
+func create_edges() -> void:
 	randomize()
-	var vertices := []
-	for i in range(0, 3):
-		var x: float = rand_range(0, max_x)
-		var y: float = rand_range(0, max_y)
-		vertices.append(Vector2(x, y))
-
-	for i in range(0, len(vertices)):
-		graph.add_edge(vertices[i-1], vertices[i])
+	for i in range(0, 6):
+		var x1: float = rand_range(0, max_x)
+		var y1: float = rand_range(0, max_y)
+		var x2: float = rand_range(0, max_x)
+		var y2: float = rand_range(0, max_y)
+		# Maybe we could avoid intersections
+		graph.add_edge(Vector2(x1, y1), Vector2(x2, y2))
 
 func intersect(a: Line2D, b: Line2D) -> Dictionary:
 	var a0: Vector2 = a.get_point_position(0)
@@ -33,7 +32,6 @@ func intersect(a: Line2D, b: Line2D) -> Dictionary:
 	var sb: Vector2 = b1 - b0
 	var cross: float = sa.cross(sb)
 
-	# TODO: Handled cross == 0
 	if abs(cross) <= 0:
 		return {'intersect': false}
 
@@ -42,20 +40,35 @@ func intersect(a: Line2D, b: Line2D) -> Dictionary:
 	var p: float = sb.cross(ba) / cross
 
 	var intersect: bool = p >= 0 && p <= 1 && q >= 0 && q <= 1
-	return {'intersect': intersect, 'res': Vector2(p, q)}
+	return {'intersect': intersect, 'point': lerp(a0, a1, p)}
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta) -> void:
 	randomize()
+	# Creates new random line
 	var candidate := Line2D.new()
 	for i in range(0, 2):
 		var x: float = rand_range(0, max_x)
 		var y: float = rand_range(0, max_y)
 		candidate.add_point(Vector2(x, y))
 
-	var edges: Array = graph.get_edges()
-	var random_edge: Line2D = edges[randi() % edges.size()]
+	# Finds intersection with existing lines
+	# If we want we can customize it so that it stops after
+	# two intersections are found
+	var intersections := {}
+	for edge in graph.get_edges():
+		var i: Dictionary = intersect(candidate, edge)
+		if i.get('intersect'):
+			intersections[edge] = i
 
-	intersect(candidate, random_edge)
+	# Splits intersected edges
+	for edge in intersections:
+		var point: Vector2 = intersections[edge].get('point')
+		graph.add_edge(edge.get_point_position(0), point)
+		graph.add_edge(point, edge.get_point_position(1))
+		graph.remove_edge(edge)
 
-
+	# Creates new edge from candidate line
+	var values: Array = intersections.values()
+	for i in range(0, len(values)):
+		graph.add_edge(values[i-1].get('point'), values[i].get('point'))
